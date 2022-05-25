6f385ae358e172622f29e0b5df50526ca339d661
nikos <nikos@nikos.com>
2022-05-25 14:20:38 +0200

    przelicznikWPF combobox Rodzaje

---
+++ Pierwszy projekt/PrzelicznikWPF/Model/Repository/DBDane.cs
@@ -0,0 +1,74 @@
+﻿using PrzelicznikWPF.Model.BazaDanych.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzelicznikWPF.Model.Repository
+{
+    class DBDane
+    {
+        private DataBaseContext bazaDanych;
+
+        public DBDane()
+        {
+            bazaDanych = new DataBaseContext();
+        }
+
+        public List<Rodzaj> GetRodzaje()
+        {
+            return bazaDanych.Rodzaj.ToList();
+        }
+
+        #region Uzupelnianie Bazy Danych
+        private void UzupelnianieBazyDanych()
+        {
+            DodanieRodzajuDoBazy("Masa");
+            DodanieJednostkiDoBazy("kilogram", "kg", 2);
+            DodanieJednostkiDoBazy("gram", "g", 2);
+            DodaniePrzelicznikaDoBazy(3, 4, 1000);
+            DodaniePrzelicznikaDoBazy(4, 3, 0.001);
+
+            DodanieRodzajuDoBazy("Waluta");
+            DodanieJednostkiDoBazy("dolar", "$", 1);
+            DodanieJednostkiDoBazy("złoty polski", "zł", 1);
+            DodaniePrzelicznikaDoBazy(2, 1, 0.24);
+            DodaniePrzelicznikaDoBazy(2, 1, 0.24);
+        }
+
+        private void DodanieRodzajuDoBazy(string nazwa)
+        {
+            Rodzaj rodzaj = new Rodzaj();
+            rodzaj.Nazwa = nazwa;
+
+            bazaDanych.Rodzaj.Add(rodzaj);
+            bazaDanych.SaveChanges();
+        }
+
+        private void DodanieJednostkiDoBazy(string nazwa, string symbol, int rodzajId)
+        {
+            Jednostki jednostki = new Jednostki();
+            jednostki.Nazwa = nazwa;
+            jednostki.Symbol = symbol;
+            jednostki.RodzajId = rodzajId;
+
+            bazaDanych.Jednostki.Add(jednostki);
+            bazaDanych.SaveChanges();
+        }
+
+        private void DodaniePrzelicznikaDoBazy(int jednostkaZrodlowaId, int jednostkaDocelowaId, double wartosc)
+        {
+            Przeliczniki przeliczniki = new Przeliczniki();
+            przeliczniki.JednostkaZrodlowaId = jednostkaZrodlowaId;
+            przeliczniki.JednostkaDocelowaId = jednostkaDocelowaId;
+            przeliczniki.Wartosc = wartosc;
+
+            bazaDanych.Przeliczniki.Add(przeliczniki);
+            bazaDanych.SaveChanges();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Pierwszy projekt/PrzelicznikWPF/ViewModel/DBDaneVM.cs
@@ -0,0 +1,51 @@
+﻿using PrzelicznikWPF.Model.BazaDanych.Model;
+using PrzelicznikWPF.Model.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilitisWPF;
+
+namespace PrzelicznikWPF.ViewModel
+{
+    class DBDaneVM : ObserverVM
+    {
+        private DBDane dane;
+
+        private List<Rodzaj> _listaRodzaj;
+        public List<Rodzaj> ListaRodzaj {
+            get
+            {
+                return _listaRodzaj;
+            }
+            set
+            {
+                _listaRodzaj = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Rodzaj _wybranyRodzaj;
+        public Rodzaj WybranyRodzaj {
+            get
+            {
+                return _wybranyRodzaj;
+            }
+            set
+            {
+                _wybranyRodzaj = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DBDaneVM()
+        {
+            dane = new DBDane();
+            ListaRodzaj = dane.GetRodzaje();
+
+            //if (WybranyRodzaj == null)
+            //    WybranyRodzaj = ListaRodzaj.First();
+        }
+    }
+}
