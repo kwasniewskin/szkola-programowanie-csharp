4e976a123b9a61a3f283ef902ef9a133d7c69b6b
Uczen <uczen@ezse.pl>
2022-10-28 11:08:45 +0200

    egzamin konsola

---
+++ WPF/WPF/EgzaminZawodowyKonsola/Osoba.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EgzaminZawodowyKonsola
+{
+    class Osoba
+    {
+        private int id;
+        private string imie;
+        public static int liczbaInstancjiKlasy = 0;
+
+        public Osoba()
+        {
+            id = 0;
+            imie = "";
+            liczbaInstancjiKlasy++;
+        }
+
+        public Osoba(int id, string imie)
+        {
+            this.id = id;
+            this.imie = imie;
+            liczbaInstancjiKlasy++;
+        }
+
+        public Osoba(Osoba osoba)
+        {
+            id = osoba.id;
+            imie = osoba.imie;
+            liczbaInstancjiKlasy++;
+        }
+
+        public void WypiszImie(string imieWejsciowe)
+        {
+            if (imie != "")
+                Console.WriteLine($"Cześć {imieWejsciowe}, mam na imię {imie}");
+            else
+                Console.WriteLine("Brak danych");
+        }
+    }
+}
---
+++ WPF/WPF/EgzaminZawodowyKonsola/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EgzaminZawodowyKonsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.liczbaInstancjiKlasy}");
+
+            Osoba osoba1 = new Osoba();
+
+            Console.WriteLine("Wprowadz id osoby: ");
+            string id = Console.ReadLine();
+            int.TryParse(id, out int intId);
+
+            Console.WriteLine("Wprowadz imie osoby: ");
+            string imie = Console.ReadLine();
+
+            Osoba osoba2 = new Osoba(intId, imie);
+
+            Osoba osoba3 = new Osoba(osoba2);
+
+            osoba1.WypiszImie("Jan");
+            osoba2.WypiszImie("Jan");
+            osoba3.WypiszImie("Jan");
+
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.liczbaInstancjiKlasy}");
+        }
+    }
+}
