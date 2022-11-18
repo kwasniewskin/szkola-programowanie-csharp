a51047241a3048471413c4b9fe820632f0df49ef
Uczen <uczen@ezse.pl>
2022-11-18 07:55:28 +0100

    flashlight app

---
+++ WPF/WPF/FlashLightXamarin/FlashLightXamarin/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace FlashLightXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TurnOnFlashlightAsync(object sender, EventArgs e)
+        {
+            await Flashlight.TurnOnAsync();
+        }
+
+        private async void TurnOffFlashlightAsync(object sender, EventArgs e)
+        {
+            await Flashlight.TurnOffAsync();
+        }
+    }
+}
