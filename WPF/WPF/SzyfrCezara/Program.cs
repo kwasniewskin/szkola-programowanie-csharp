857901971043ea1edabd990a1d5e73e5abc936aa
Uczen <uczen@ezse.pl>
2022-12-01 16:05:33 +0100

    first asp

---
+++ WPF/WPF/SzyfrCezara/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SzyfrCezara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int przesuniecie;
+
+            Console.WriteLine("Podaj wyraz do zaszyfrowania: ");
+            string wyraz = Console.ReadLine();
+            Console.WriteLine("Podaj przesuniecie:");
+            string przesuniecieString = Console.ReadLine();
+            int.TryParse(przesuniecieString, out przesuniecie);
+
+            void Szyfrowanie()
+            {
+                char[] wyrazTab;
+
+                for(int i = 0; 1 < wyraz.Length; i++)
+                {
+                    wyraz[i]
+
+                }
+            }
+
+            void Deszyfrowanie()
+            {
+
+            }
+        }
+    }
+}
