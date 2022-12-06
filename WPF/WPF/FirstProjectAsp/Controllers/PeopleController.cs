1657a0c108e18117cdccaa1d7778249d04ca4210
Uczen <uczen@ezse.pl>
2022-12-06 13:14:25 +0100

    commituje do gita

--- WPF/WPF/FirstProjectAsp/Controllers/PeopleController.cs
+++ WPF/WPF/FirstProjectAsp/Controllers/PeopleController.cs
@@ -34,7 +34,69 @@ namespace FirstProjectAsp.Controllers
             });
             peopleDBContext.SaveChanges();
 
-            return View("Index");
+            return RedirectToAction("ShowAllPersons");
+        }
+
+        [HttpGet]
+        public IActionResult EditPerson(int id)
+        {
+            PersonDTO personDTO = peopleDBContext.Persons.
+                Where(x => x.Id == id).
+                Select(x => new PersonDTO()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Surname = x.Surname,
+                    Age = x.Age
+                }).
+                FirstOrDefault();
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult EditPerson(PersonDTO personDTO)
+        {
+            Person person = peopleDBContext.Persons.FirstOrDefault(x => x.Id == personDTO.Id);
+
+            if (person == null)
+                return NotFound();
+
+            person.Name = personDTO.Name;
+            person.Surname = personDTO.Surname;
+            person.Age = personDTO.Age;
+            peopleDBContext.SaveChanges();
+
+            return RedirectToAction("ShowAllPersons");
+        }
+
+        public IActionResult DeletePerson(int id)
+        {
+            Person person = peopleDBContext.Persons.FirstOrDefault(x => x.Id == id);
+
+            if (person == null)
+                return NotFound();
+
+            peopleDBContext.Persons.Remove(person);
+            peopleDBContext.SaveChanges();
+
+            return RedirectToAction("ShowAllPersons");
+        }
+
+        [HttpGet]
+        public IActionResult SearchPerson([FromQuery]string name)
+        {
+            List<PersonDTO> newList = peopleDBContext.Persons.
+                Where(x => string.IsNullOrWhiteSpace(name) || x.Name.Contains(name)).
+                Select(x => new PersonDTO()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Surname = x.Surname,
+                    Age = x.Age
+                }).ToList();
+
+            return View(newList);
         }
 
         public IActionResult ShowAllPersons()
@@ -64,10 +126,5 @@ namespace FirstProjectAsp.Controllers
             return View(person);
         }
 
-        //[HttpPost]
-        public IActionResult EditPerson(PersonDTO person)
-        {
-            return View(person);
-        }
     }
 }
